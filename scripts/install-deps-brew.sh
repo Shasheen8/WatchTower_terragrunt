644432fb9bd55dc27b715738d3896ae31e8ce040
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-10-02 03:40:58 -0400

    Add files via upload

---
+++ scripts/install-deps-brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+brew install terraform terragrunt packer ansible python nodejs git
+npm i -g meta
+sudo pip3 install preptools cookiecutter awscli fire
+
+# Verify
+ansible --version
+cookiecutter --version
+terragrunt -v
+terraform -v
+packer -v
+meta --version
